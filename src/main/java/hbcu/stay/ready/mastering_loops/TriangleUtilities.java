d1d9850337f18df1389df57dbafa2c7652431352
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-08 10:24:02 -0400

    1 eternity later... I can finally submit the lab lol

--- src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
@@ -1,20 +1,36 @@
 package hbcu.stay.ready.mastering_loops;
 
 public class TriangleUtilities {
-
+    
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+        for(int row = 1; row <= numberOfRows; row++){
+            result += getRow(row) + "\n";
+        }
+        return result;
     }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+    public static String getRow(int width) {
+        String result = "";
+        for(int i = 1; i <= width; i++){
+            result += "*";
+        }
+        return result;
     }
-
+    
     public static String getSmallTriangle() {
-        return null;
+        String result = "";
+        for(int row = 1; row <= 4; row++){
+            result += getRow(row) + "\n";
+        }
+        return result;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String result = "";
+        for(int row = 1; row <= 9; row++){
+            result += getRow(row) + "\n";
+        }
+        return result;
     }
 }
